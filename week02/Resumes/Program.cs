61f6fff7cbb4e987d90b77702e8e3ff49100ba87
victormba-kalu <mbavictorkalu@gmail.com>
2025-05-15 12:55:08 +0100

    completed all but the final step of resume project

--- week02/Resumes/Program.cs
+++ week02/Resumes/Program.cs
@@ -4,6 +4,29 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Resumes Project.");
+        Job job1 = new Job();
+        job1._company = "Microsoft";
+        job1._jobTitle = "Software Engineer";
+        job1._startYear = 2019;
+        job1._endYear = 2022;
+
+        job1.Display();
+
+
+        Job job2 = new Job();
+        job2._company = "Amazon";
+        job2._jobTitle = "Data Scientist";
+        job2._startYear = 2022;
+        job2._endYear = 2023;
+
+        job2.Display();
+
+        Resume myResume = new Resume();
+        myResume._jobs.Add(job1);
+        myResume._jobs.Add(job2);
+
+        //Console.WriteLine(myResume._jobs[0]._jobTitle);
     }
-}
+
+}     
+  
