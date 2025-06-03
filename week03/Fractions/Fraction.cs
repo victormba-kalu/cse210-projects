7b90da8ed48522b59e905338c4e8422745391bd9
victormba-kalu <mbavictorkalu@gmail.com>
2025-06-03 16:20:55 +0100

    basic encapsulation

---
+++ week03/Fractions/Fraction.cs
@@ -0,0 +1,47 @@
+using System;
+
+public class Fraction
+{
+    private int _top;
+    private int _bottom;
+
+    public Fraction()
+    {
+        _top = 1;
+        _bottom = 1;
+    }
+
+    public Fraction(int top)
+    {
+        _top = top;
+        _bottom = 1;
+    }
+
+    public Fraction(int top, int bottom)
+    {
+        _top = top;
+        _bottom = bottom;
+    }
+
+    public void SetFraction(int top, int bottom)
+    {
+        _top = top;
+        _bottom = bottom;
+    }
+
+    public string GetFractionString()
+    {
+        return $"{_top}/{_bottom}";
+    }
+
+    public double GetDecimalValue()
+    {
+        if (_bottom == 0)
+        {
+            throw new DivideByZeroException("The denominator cannot be zero.");
+        }
+        return (double)_top / _bottom;
+    }
+
+
+}
--- week03/Fractions/Program.cs
+++ week03/Fractions/Program.cs
@@ -4,6 +4,36 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Fractions Project.");
+        Fraction fraction1 = new Fraction();
+        Fraction fraction2 = new Fraction(6);
+        Fraction fraction3 = new Fraction(6, 7);
+
+
+        Console.WriteLine("Fraction 1: " + fraction1.GetFractionString());
+        Console.WriteLine("Fraction 2: " + fraction2.GetFractionString());
+        Console.WriteLine("Fraction 3: " + fraction3.GetFractionString());
+
+        Fraction fraction4 = new Fraction(1);
+        Fraction fraction5 = new Fraction(5);
+        Console.WriteLine("Fraction 4: " + fraction4.GetFractionString());
+        Console.WriteLine("Fraction 4: " + fraction4.GetDecimalValue());
+
+        Console.WriteLine("Fraction 5: " + fraction5.GetFractionString());
+        Console.WriteLine("Fraction 5: " + fraction5.GetDecimalValue());
+
+        fraction4.SetFraction(3, 4);
+        fraction5.SetFraction(1, 3);
+
+        
+
+        Console.WriteLine("Fraction 4: " + fraction4.GetFractionString());
+        Console.WriteLine("Fraction 4: " + fraction4.GetDecimalValue());
+
+        Console.WriteLine("Fraction 5: " + fraction5.GetFractionString());
+        Console.WriteLine("Fraction 5: " + fraction5.GetDecimalValue());
+
+        
+
     }
+
 }
