bd15e5731ea94734e66a680348abfc01ed763dd8
victormba-kalu <mbavictorkalu@gmail.com>
2025-06-05 14:10:25 +0100

    created private variables for the class

---
+++ week04/OnlineOrdering/Address.cs
@@ -0,0 +1,13 @@
+using System;
+
+// Ever since my last project, I've developed the habit of including namespaces in my program. 
+namespace ProductOrderingSystem
+{
+    public class Address
+    {
+        private string _streetAddress;
+        private string _city;
+        private string _stateProvince;
+        private string _country;
+    }
+}
