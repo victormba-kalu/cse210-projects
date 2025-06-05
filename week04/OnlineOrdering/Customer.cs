fdf132d705ed715aef63af409848fcb3ebd1de67
victormba-kalu <mbavictorkalu@gmail.com>
2025-06-05 14:23:10 +0100

    created variables for the product class

--- week04/OnlineOrdering/Customer.cs
+++ week04/OnlineOrdering/Customer.cs
@@ -6,5 +6,30 @@ namespace ProductOrderingSystem
     {
         private string _name;
         private Address _address;
+
+        public Customer(string name, Address address)
+        {
+            // I use the "throw new ArgumentNullException to catch errors and prevent the code from breaking"
+            _name = name ?? throw new ArgumentNullException(nameof(name), "Customer name should not be null. Enter valid name");
+            _address = address ?? throw new ArgumentNullException(nameof(address), "Customer address shouldn't be null. Enter vaild name");
+        }
+
+      
+        public string GetName()
+        {
+            return _name;
+        }
+
+       
+        public Address GetAddress()
+        {
+            return _address;
+        }
+
+        
+        public bool LivesInUSA()
+        {
+            return _address.IsInUSA(); 
+        }
     }
 }
---
+++ week04/OnlineOrdering/Product.cs
@@ -0,0 +1,13 @@
+using System;
+
+// I love using namespaces
+namespace ProductOrderingSystem
+{
+    public class Product
+    {
+        private string _name;
+        private string _productId; 
+        private double _pricePerUnit;
+        private int _quantity;
+    }
+}
