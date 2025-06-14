8ca4b806cdabcd58eea95984da882331d772908d
victormba-kalu <mbavictorkalu@gmail.com>
2025-06-14 22:41:47 +0100

    added the getListFromUser method to the Listing activity

--- week05/Mindfulness/ListingActivity.cs
+++ week05/Mindfulness/ListingActivity.cs
@@ -1,81 +1,86 @@
 using System;
 using System;
 using System.Threading;
-using System.Collections.Generic; 
+using System.Collections.Generic;
 namespace MindfulnessActivity
 
 {
-    
+
 
     public class ListingActivity : Activity
     {
+        
         private List<string> _prompts;
-        private int _count; 
-        private Random _random;
 
-        // Constructor
+        
         public ListingActivity() : base("Listing Activity", "This activity will help you reflect on the good things in your life by having you list as many things as you can in a certain area.")
         {
-            _prompts = new List<string>
-        {
-            "Who are people that you appreciate?",
-            "What are personal strengths of yours?",
-            "Who are people that you have helped this week?",
-            "When have you felt the Holy Ghost this month?",
-            "Who are some of your personal heroes?"
-        };
-            _count = 0; 
-            _random = new Random();
+                _prompts = new List<string>
+            {
+                "Who are people that you appreciate?",
+                "What are personal strengths of yours?",
+                "Who are people that have helped you this week?",
+                "When have you felt the Holy Ghost this month?",
+                "Who are some of your personal heroes?",
+                "What are some things you are grateful for?",
+                "What are your favorite hobbies and why?",
+                "What challenges have you overcome recently?",
+                "What skills are you proud of having developed?",
+                "List things that make you feel peaceful."
+            };
         }
 
-     
         public void Run()
         {
             DisplayStartingMessage(); 
 
-            Console.Clear();
-            Console.WriteLine("Ponder on the following prompt:");
-            Console.WriteLine();
-            DisplayPrompt(); 
-            Console.WriteLine();
+            Console.WriteLine("\nThis activity will help you reflect on the good things in your life by having you list as many things as you can in a certain area.");
+            Console.WriteLine("Think about the following prompt:");
 
-            Console.WriteLine("You may begin listing items in: ");
+            string prompt = GetRandomPrompt(); 
+            Console.WriteLine($"--- {prompt} ---"); 
+
+            Console.Write("You may begin in: ");
             ShowCountDown(5); 
-
             Console.WriteLine(); 
-            Console.WriteLine("Start listing items now (press Enter after each item):");
 
-            DateTime startTime = DateTime.Now;
-            DateTime endTime = startTime.AddSeconds(_duration);
-            _count = 0; 
-        
-            while (DateTime.Now < endTime)
-            {
-                if (Console.KeyAvailable) 
-                {
-                    string item = Console.ReadLine();
-                    
-                    _count++;
-                }
-            }
+            Console.WriteLine("\nStart listing items now (press Enter after each item):");
 
-            Console.WriteLine($"\nYou listed {_count} items!");
-            ShowSpinner(3); 
+            List<string> userItems = GetListFromUser(); 
+
+            Console.WriteLine($"\nYou listed {userItems.Count} items.");
 
             DisplayEndingMessage(); 
         }
 
-        private void DisplayPrompt()
+        // This method returns a random prompt from the list of prompts.
+        public string GetRandomPrompt()
         {
-            string prompt = GetRandomPrompt();
-            Console.WriteLine($"--- {prompt} ---");
+            Random random = new Random();
+            int index = random.Next(_prompts.Count);
+            return _prompts[index];
         }
 
-     
-        private string GetRandomPrompt()
+        // This method collects a list of items from the user until the specified duration is reached.
+        public List<string> GetListFromUser()
         {
-            int index = _random.Next(0, _prompts.Count);
-            return _prompts[index];
+            List<string> items = new List<string>();
+            DateTime startTime = DateTime.Now;
+            DateTime endTime = startTime.AddSeconds(_duration);
+
+            while (DateTime.Now < endTime)
+            {
+                Console.Write("> ");
+                
+                string item = Console.ReadLine();
+
+                if (!string.IsNullOrWhiteSpace(item))
+                {
+                    items.Add(item);
+                }
+          
+            }
+            return items;
         }
     }
 }
